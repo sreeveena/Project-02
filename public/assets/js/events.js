e10445b25376ebd6b16c9f9ffcc16b88669240b2
sreeveena <sreeveena21@gmail.com>
2020-01-17 21:51:39 -0800

    created dynamic table for events

--- public/assets/js/events.js
+++ public/assets/js/events.js
@@ -24,12 +24,41 @@ function getEvents(lat, lon) {
         data: {lon: lon, lat:lat, date:startDate}
     }).then(function (response){
         // console.log(response);
-        for(var i = 0; i<10; i++){
-            $(".events").html(response.results[0].assetName);
-            var table = "";
-        }
-        
-        
+        createEventsTable(response);
     });
 }
 getLocation();
+// ---------------------------Create Events Table --------------------------
+function createEventsTable(data){
+    var mybody = document.getElementsByTagName("body")[0];
+    var mytable = document.createElement("table");
+    mytable.setAttribute("id","eventsTable");
+    var mytablebody = document.createElement("tbody");
+    mytablebody.setAttribute("id","eventsTBody");
+    for(var j = 0; j < data.results.length; j++) {
+        // creates a <tr> element
+        mycurrent_row = document.createElement("tr");
+        mycurrent_row.setAttribute("id","eventsTr");
+        // creates a <td> element
+        mycurrent_cell = document.createElement("td");
+        mycurrent_cell.setAttribute("class","eventsTd");
+        // creates a Text Node
+        currenttext = document.createTextNode(data.results[j].assetName);
+        //create registration button
+        button = document.createElement("button");
+        button.innerHTML = "Register";
+        button.setAttribute("class","eventsButton");
+        button_cell = document.createElement("td");
+        // appends the Text Node we created into the cell <td>
+        mycurrent_cell.appendChild(currenttext);
+        // appends the cell <td> into the row <tr>
+        mycurrent_row.appendChild(mycurrent_cell);
+        button_cell.appendChild(button);
+        mycurrent_row.appendChild(button_cell);
+        // appends the row <tr> into <tbody>
+        mytablebody.appendChild(mycurrent_row);
+    }
+    mytable.appendChild(mytablebody);
+    // appends <table> into <body>
+    mybody.appendChild(mytable);
+}
