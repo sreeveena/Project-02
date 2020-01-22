6c033353f05501fb04e0b96989fe453263362f71
Dave Conlan <daveconlan@me.com>
2020-01-22 10:23:14 -0800

    adding js files

---
+++ js/fe.js
@@ -0,0 +1,11 @@
+function clickAccordion () {
+    document.getElementById("one").innerHTML =" Description One";
+    document.getElementById("one").style.fontsize = "20px";
+    document.getElementById("one").style.paddingTop = "1%";
+    document.getElementById("one").style.paddingLeft = "1%";
+    document.getElementById("one").style.paddingRight = "1%";
+    document.getElementById("one").style.backgroundColor = "#007fff"; 
+
+}
+clickAccordion (); 
+
