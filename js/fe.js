796ceab3fd4dcfe9ef7e2caf2555697baea59ecd
Dave Conlan <daveconlan@me.com>
2020-01-22 11:42:02 -0800

    workingon the js

--- js/fe.js
+++ js/fe.js
@@ -1,11 +1,4 @@
-function clickAccordion () {
-    document.getElementById("one").innerHTML =" Description One";
-    document.getElementById("one").style.fontsize = "20px";
-    document.getElementById("one").style.paddingTop = "1%";
-    document.getElementById("one").style.paddingLeft = "1%";
-    document.getElementById("one").style.paddingRight = "1%";
-    document.getElementById("one").style.backgroundColor = "#007fff"; 
 
-}
-clickAccordion (); 
-
+function myFunction() { 
+    document.getElementById("demo").innerHTML = "Hello JavaScript!";
+  }
