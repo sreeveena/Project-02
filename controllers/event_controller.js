14fe083a041a9221e0981901623d7dee05943f81
sreeveena <sreeveena21@gmail.com>
2020-01-22 18:11:13 -0800

    search by location and order.

--- controllers/event_controller.js
+++ controllers/event_controller.js
@@ -6,9 +6,9 @@ router.post("/api/events", function(req, res) {
     var startDate = req.body.date;
     var lon = req.body.lon;
     var lat = req.body.lat;
-
-    var queryURL = "http://api.amp.active.com/v2/search?query=running&category=event&start_date="+
-    startDate+"..&lat_lon="+lat +","+lon +"&radius=50&api_key=9eqk4qg7mf27c4qwe5vxd79r";
+    
+    var queryURL = "http://api.amp.active.com/v2/search?category=event&start_date="+
+    startDate+"..&lat_lon="+lat +","+lon +"&radius=20&sort=distance&api_key=9eqk4qg7mf27c4qwe5vxd79r";
     
     console.log(queryURL);
     request(queryURL,{json: true}, function(err,result,body){
@@ -19,5 +19,36 @@ router.post("/api/events", function(req, res) {
     });
     
 });
+
+router.post("/api/levents", function(req, res) {
+    var startDate = req.body.date;
+    var cityAndState = req.body.city;
+    
+    var queryURL = "http://api.amp.active.com/v2/search?category=event&start_date="+
+    startDate+"..&near="+cityAndState +"&radius=20&sort=distance&api_key=9eqk4qg7mf27c4qwe5vxd79r";
+    console.log(queryURL);
+    request(queryURL,{json: true}, function(err,result,body){
+        if(err)
+        return console.log(err);
+        // console.log(body);
+        res.json (body);
+    });
+});
+
+router.post("/api/qevents", function(req, res) {
+    var startDate = req.body.date;
+    var query = req.body.query;
+   
+    var queryURL = "http://api.amp.active.com/v2/search?query="+query+"&category=event&start_date="+
+    startDate+"..&radius=20&sort=distance&api_key=9eqk4qg7mf27c4qwe5vxd79r";
+    console.log(queryURL);
+    request(queryURL,{json: true}, function(err,result,body){
+        if(err)
+        return console.log(err);
+        // console.log(body);
+        res.json (body);
+    });
+});
+
 
 module.exports = router;
--- public/assets/js/events.js
+++ public/assets/js/events.js
@@ -1,13 +1,57 @@
+$(function() {
+    $("#searchLocation").on("keypress", function(e) {
+        
+        if(e.which === 13){
+            $.ajax({
+                url: "/api/levents",
+                method: "POST",
+                data: {city:$("#searchLocation").val(), date: todaysDate()}
+            }).then(function (response){
+                // console.log(response);
+                createEventsTable(response);
+                
+            });
+        }
+        return;
+        // var email = document.getElementById("email").value;
+        // var password = document.getElementById("psw").value;
+  
+        // console.log("user entered email:" + document.getElementById("email").value);
+        // console.log("user entered password:" +document.getElementById("psw").value);
+        // registerUser(email,password, "events");
+        // $('#reg').find('input').val('');
+    });
+});
+
+$(function() {
+    $("#searchEvent").on("keypress", function(e) {
+       
+        if(e.which === 13){
+            $.ajax({
+                url: "/api/qevents",
+                method: "POST",
+                data: {query:$("#searchEvent").val(), date: todaysDate()}
+            }).then(function (response){
+                // console.log(response);
+                createEventsTable(response);
+                
+            });
+        }
+        return;
+    });
+});
+
 function getLocation() {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(function(position) {
         getEvents(position.coords.latitude, position.coords.longitude);
+        
       });
     } else {
       loc.innerHTML = "Geolocation is not supported by this browser.";
     }
   }
-function getEvents(lat, lon) {
+  function todaysDate(){
     var today =  new Date();
     var dd = today.getDate();
     var mm = today.getMonth()+1;
@@ -17,19 +61,31 @@ function getEvents(lat, lon) {
     if(mm<10)
         mm=`0${mm}`;
     var startDate = `${yyyy}-${mm}-${dd}`;
-
+    return startDate;
+  }
+function getEvents(lat, lon) {
     $.ajax({
         url: "/api/events",
         method: "POST",
-        data: {lon: lon, lat:lat, date:startDate}
+        data: {lon: lon, lat:lat, date:todaysDate()}
     }).then(function (response){
         // console.log(response);
         createEventsTable(response);
+        $.ajax({
+            url: "https://geocode.xyz/" + lat + "," + lon + "?json=1",
+            method: "GET",
+        }).then(function (response){
+            console.log(response);
+            console.log(response.city, response.state);
+            $("#searchLocation").val(response.city + "," + response.state);
+        });
     });
 }
 getLocation();
+
 // ---------------------------Create Events Table --------------------------
 function createEventsTable(data){
+    $("#eventsTable").remove();
     var mybody = document.getElementsByTagName("body")[0];
     var mytable = document.createElement("table");
     mytable.setAttribute("id","eventsTable");
@@ -40,18 +96,39 @@ function createEventsTable(data){
         mycurrent_row = document.createElement("tr");
         mycurrent_row.setAttribute("id","eventsTr");
         // creates a <td> element
+        date_cell = document.createElement("td");
+        date_cell.setAttribute("class","eventsDate");
+
+        eventDate = document.createTextNode(formatedDate(data.results[j].activityStartDate));
+        // eventDate = document.createTextNode(data.results[j].activityStartDate);
+
+
         mycurrent_cell = document.createElement("td");
         mycurrent_cell.setAttribute("class","eventsTd");
+        
         // creates a Text Node
         currenttext = document.createTextNode(data.results[j].assetName);
+        addresstext = document.createTextNode(data.results[j].place.placeName + " , "+
+        data.results[j].place.cityName+ " , "+ data.results[j].place.stateProvinceCode);
+        //http://api.amp.active.com/v2/search?asset.assetGuid=7e0cbff6-98ce-4a64-addf-4de5a85b78c8&api_key=9eqk4qg7mf27c4qwe5vxd79r
+        // use the above url query format for querying only this event for registeration.
+        //  the asset guid is unique for event.
+        assetGuidText = document.createElement("div");
+        assetGuidText.setAttribute("id","guid"+j);
+        assetGuidText.innerHTML = data.results[j].assetGuid;
+        assetGuidText.style.display = "none";
         //create registration button
         button = document.createElement("button");
         button.innerHTML = "Register";
         button.setAttribute("class","eventsButton");
         button_cell = document.createElement("td");
+        date_cell.appendChild(eventDate);
         // appends the Text Node we created into the cell <td>
         mycurrent_cell.appendChild(currenttext);
+        mycurrent_cell.appendChild(assetGuidText);
+        mycurrent_cell.appendChild(addresstext);
         // appends the cell <td> into the row <tr>
+        mycurrent_row.appendChild(date_cell);
         mycurrent_row.appendChild(mycurrent_cell);
         button_cell.appendChild(button);
         mycurrent_row.appendChild(button_cell);
@@ -61,4 +138,23 @@ function createEventsTable(data){
     mytable.appendChild(mytablebody);
     // appends <table> into <body>
     mybody.appendChild(mytable);
+    
 }
+
+function formatedDate(date){
+console.log(date);
+// 2020-03-17T22:15:00
+    date = date.substring(0, 10);
+    console.log(date);
+    d = new Date(date);
+    formatedStr = getMonthString(d.getMonth()) + "  "+ d.getDate()+ "  "+d.getFullYear();
+    return formatedStr;
+}
+function getMonthString(num){
+    const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "June",
+  "July", "Aug", "Sep", "Oct", "Nov", "Dec"
+];
+return monthNames[num];
+}
+
+
