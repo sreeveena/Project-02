9ff83a1fcf24910c9b603ee0a3f74079e7ffcd7d
Dave Conlan <daveconlan@me.com>
2020-01-22 20:55:54 -0800

    saving html changes

--- js/fe.js
+++ js/fe.js
@@ -1,4 +1,19 @@
-
-function myFunction() { 
-    document.getElementById("demo").innerHTML = "Hello JavaScript!";
-  }
+function register(event) {
+    $('#myModal').modal('toggle')
+    const data = {
+        event1: {
+            FirstName: 'FirstName',
+            LastName: 'Last Name'
+        },
+        event2: {
+            title: 'Second event',
+            body: 'body2'
+        },
+        event3: {
+            title: 'Third event',
+            body: 'body3'
+        },
+    }[event]
+    $('.modal-title').text(data.title)
+    $('.modal-body').text(data.body)
+}
