c8d3029bbe14ba4307af2f5b7fc1f3c57770059c
Sree <sreeveena21@gmail.com>
2020-01-22 21:50:55 -0800

    pull request with out package-lock.json (#3)
    * checking git status
    
    * basic file structure.
    
    * initial code for authentication
    
    * sign in and register directly through the site.
    
    * adding session to the signin user.
    
    * enabled google signin
    
    * worked on signout
    
    * get all the events from active
    
    * get all the events from active
    
    * validation for password and email.
    
    * created dynamic table for events
    
    * updating the images
    
    * updating the images
    
    * image path set in html.
    
    * encrypted the password in database - one way hash
    
    * search by location and order.
    
    * delete package-lock.json.
    
    * small changes to the search query

--- controllers/event_controller.js
+++ controllers/event_controller.js
@@ -6,9 +6,9 @@ router.post("/api/events", function(req, res) {
     var startDate = req.body.date;
     var lon = req.body.lon;
     var lat = req.body.lat;
-
-    var queryURL = "http://api.amp.active.com/v2/search?query=running&category=event&start_date="+
-    startDate+"..&lat_lon="+lat +","+lon +"&radius=50&api_key=9eqk4qg7mf27c4qwe5vxd79r";
+    var query = req.body.query;
+    var queryURL = "http://api.amp.active.com/v2/search?category=event&radius=20&sort=distance&api_key=9eqk4qg7mf27c4qwe5vxd79r&start_date="+
+    startDate+"..&"+query;
     
     console.log(queryURL);
     request(queryURL,{json: true}, function(err,result,body){
@@ -16,8 +16,7 @@ router.post("/api/events", function(req, res) {
         return console.log(err);
         // console.log(body);
         res.json (body);
-    });
-    
+    });   
 });
 
 module.exports = router;
