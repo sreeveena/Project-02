e62cda24e59059a0bac09982fb4178084887be3a
sreeveena <sreeveena21@gmail.com>
2020-01-26 11:37:14 -0800

    added user event registration tables

--- db/schema.sql
+++ db/schema.sql
@@ -8,3 +8,17 @@ CREATE TABLE users (
   provider varchar(30) NOT NULL,
   PRIMARY KEY(id)
 );
+
+CREATE TABLE registered_events (
+  id int AUTO_INCREMENT,
+  guid varchar(100) NOT NULL,
+  name varchar(100) NOT NULL,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE user_registrations (
+  user_id int NOT NULL,
+  event_id int NOT NULL,
+  reg_date DATE NOT NULL,
+  PRIMARY KEY(id)
+);
